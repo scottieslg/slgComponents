9e8209ae9ac86d858e4f25e07d01ce6f4087d9d0
Scott Gines <scottieslg@hotmail.com>
2016-08-23 11:18:00 -0600

    Added code for npm publish

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+﻿var gulp = require("gulp");
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var debug = require('gulp-debug');
+
+
+gulp.task('default', ['js', 'css']);
+
+gulp.task('js', function () {
+    return gulp.src(['scripts/slgSpinner.js', 'scripts/slgComponents.js', 'scripts/slgButtonSpinner.js'])
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+        .pipe(rename('slgComponents.min.js'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('npm/dist/js'))
+});
+
+gulp.task('css', function () {
+    return gulp.src(['content/slgButtonSpinner.css'])
+		.pipe(uglifycss())
+        .pipe(rename('slgComponents.min.css'))
+		.pipe(gulp.dest('npm/dist/css'))
+});
