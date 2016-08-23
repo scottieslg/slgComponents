3a2b58d8c45b2547db70d3060627204bd0b7c350
Scott Gines <scottieslg@hotmail.com>
2016-08-23 10:24:41 -0600

    Initial add

---
+++ scripts/slgButtonSpinner.js
@@ -0,0 +1,70 @@
+﻿angular.module('slgComponents')
+.directive('slgButtonSpinner', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'EA',
+        scope: {},
+        link: function (scope, element, attr) {
+            scope.showing = false;
+
+            element.append(angular.element("<div id='slgButtonSpinner_spinner_" + scope.$id + "' class='slgButtonSpinner-Spinner'></div>"));
+
+            if (attr["slgButtonSpinner"]) {
+                scope.$watch('$parent.' + attr["slgButtonSpinner"], function (newVal, oldVal) {
+                    if (oldVal === newVal)
+                        return;
+
+                    if (newVal === true && scope.showing === false) 
+                        scope.showSpinner();
+                    else if (newVal == false && scope.showing === true)
+                        scope.hideSpinner();                    
+                });
+            }
+
+            scope.showSpinner = function () {
+                scope.showing = true;
+
+                element.removeClass('slgButtonSpinnerCollapse');
+                element.addClass('slgButtonSpinnerGrow');
+                element.addClass('right');
+
+                var target = document.getElementById('slgButtonSpinner_spinner_' + scope.$id);
+
+                var height = target.clientHeight;
+                console.log(height);
+
+                if (height <= 12) {
+                    var opts = {
+                        radius: 3,
+                        length: 3,
+                        width: 1
+                    }
+                }
+                else if (height <= 14) {
+                    var opts = {
+                        radius: 4,
+                        length: 4,
+                        width: 2
+                    }
+                }
+                else {
+                    var opts = {
+                        radius: 5,
+                        length: 5,
+                        width: 3
+                    }
+                }
+                var spinner = new slgSpinner(opts).spin(target);
+            }
+
+            scope.hideSpinner = function () {
+                element.removeClass('slgButtonSpinnerGrow');
+                element.addClass('slgButtonSpinnerCollapse');
+
+                scope.showing = false;
+            }
+        },
+        controller: ['$scope', function ($scope) {
+
+        }]
+    }
+}]);
